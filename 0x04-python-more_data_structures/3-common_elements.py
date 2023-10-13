5bfa99347d1a149b31e2a87eb760f19b7c9f4c61
zak-github <elmyar.zakaria@gmail.com>
2023-10-13 16:21:15 -0700

    done

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,17 +1,9 @@
+#!/usr/bin/python3
+
+
 def common_elements(set_1, set_2):
     """
-    Returns a set of common elements in two sets.
+    A function that returns a set of
+    common elements in two sets
     """
-    common_set = set()
-    for element in set_1:
-        if element in set_2:
-            common_set.add(element)
-    return common_set
-
-
-
-
-
-
-
-
+    return (set_1 & set_2)
