709e34af710b6ed09dc7431df7694fac7bcf8e32
zak-github <elmyar.zakaria@gmail.com>
2023-10-23 16:32:08 -0700

    tasks 245 0x05. Python - Exceptions done 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,23 +1,21 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    # Initialize a variable to keep track of the number of elements printed
-    elements_printed = 0
+    """Print x elememts of a list.
 
-    # Use a for loop to iterate through the list
-    for element in my_list:
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
         try:
-            # Try to print the element
-            print(element, end=" ")
-            elements_printed += 1
-        except:
-            # If an exception occurs (e.g., when the element is not printable), continue to the next element
-            continue
-
-        # Check if we have printed 'x' elements, if so, break the loop
-        if elements_printed >= x > 0:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
             break
-
-    # Print a newline character to end the line
-    print()
-
-    # Return the real number of elements printed
-sh: 1: q: not found
+    print("")
+    return (ret)
